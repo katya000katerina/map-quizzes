08d5b1029e019caab32ffa55e9f4630642943c2f
Katerina <krasnvasilk@gmail.com>
2024-06-13 18:12:14 +0300

    Add a proper exception handling for sending 401 and 403 HTTP status codes

--- src/main/java/com/mapquizzes/config/SecurityConfig.java
+++ src/main/java/com/mapquizzes/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.mapquizzes.config;
 
+import com.mapquizzes.security.CustomAccessDeniedHandler;
 import com.mapquizzes.security.JwtAuthenticationFilter;
 import com.mapquizzes.security.JwtLogoutHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +19,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
@@ -27,6 +30,7 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtFilter;
     private final JwtLogoutHandler jwtLogoutHandler;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,6 +43,8 @@ public class SecurityConfig {
                         .requestMatchers("/", "/api/v1/quizzes/**", "/api/v1/quizzes-questions/**").permitAll()
                         .anyRequest().authenticated())
                 .userDetailsService(userDetailsService)
+                .exceptionHandling(e -> e.accessDeniedHandler(customAccessDeniedHandler)
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(l -> l.logoutUrl("/api/v1/auth/logout")
