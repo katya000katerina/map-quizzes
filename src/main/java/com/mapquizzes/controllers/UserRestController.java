8cbacdb54cc621ef639a3b58e52347cfa5c52452
Katerina <krasnvasilk@gmail.com>
2024-06-13 17:38:51 +0300

    Add a rest controller for a User entity

---
+++ src/main/java/com/mapquizzes/controllers/UserRestController.java
@@ -0,0 +1,23 @@
+package com.mapquizzes.controllers;
+
+import com.mapquizzes.models.dto.UserDto;
+import com.mapquizzes.services.interfaces.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserRestController {
+    private final UserService service;
+
+    @GetMapping("/current")
+    public ResponseEntity<UserDto> getSignedInUser(Principal user) {
+        return ResponseEntity.ok(service.getByUsername(user.getName()));
+    }
+}
