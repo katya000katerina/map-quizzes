fb2c4c129a8e1c9f0e84d5cb0dfa93c649b5d2c3
Katerina <krasnvasilk@gmail.com>
2024-06-15 10:21:12 +0300

    Add Getter and Setter annotations

--- src/main/java/com/mapquizzes/models/entities/QuizCompletionFastestTimeEntity.java
+++ src/main/java/com/mapquizzes/models/entities/QuizCompletionFastestTimeEntity.java
@@ -3,10 +3,14 @@ package com.mapquizzes.models.entities;
 import com.mapquizzes.models.entities.compositekeys.QuizCompletionFastestTimeId;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(schema = "quizzes", name = "quizzes_completion_fastest_time")
 @IdClass(QuizCompletionFastestTimeId.class)
+@Getter
+@Setter
 public class QuizCompletionFastestTimeEntity {
     @Id
     @Column(name = "user_id")
