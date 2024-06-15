bf0280463cf7d55c5a610774af28c83eaad381f7
Katerina <krasnvasilk@gmail.com>
2024-06-15 10:12:10 +0300

    Refactor

--- src/main/resources/static/js/refresh_token.js
+++ src/main/resources/static/js/refresh_token.js
@@ -1,12 +1,12 @@
-export async function fetchWithAuth(uri, options) {
-    const response = await fetch(uri, options);
+export async function fetchWithAuth(path, requestInit) {
+    const response = await fetch(path, requestInit);
     const status = response.status;
     if (status !== 401) {
         return response;
     } else {
         const refreshResponse = await fetch('/api/v1/auth/refresh-token', {method : 'POST'});
         if (refreshResponse.ok){
-            return await fetch(uri, options);
+            return await fetch(path, requestInit);
         } else{
             return response;
         }
