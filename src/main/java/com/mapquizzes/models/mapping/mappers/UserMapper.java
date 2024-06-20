fae06573b1e3cfb6114335bbe3cb18c7a8a29e7b
Katerina <krasnvasilk@gmail.com>
2024-06-20 17:08:34 +0300

    Remove UserImage mapping

--- src/main/java/com/mapquizzes/models/mapping/mappers/UserMapper.java
+++ src/main/java/com/mapquizzes/models/mapping/mappers/UserMapper.java
@@ -1,9 +1,7 @@
 package com.mapquizzes.models.mapping.mappers;
 
 import com.mapquizzes.models.dto.UserDto;
-import com.mapquizzes.models.dto.UserImageDto;
 import com.mapquizzes.models.entities.UserEntity;
-import com.mapquizzes.models.entities.UserImageEntity;
 import com.mapquizzes.models.mapping.CycleAvoidingContext;
 import com.mapquizzes.models.mapping.MapstructIgnore;
 import org.mapstruct.Context;
@@ -24,7 +22,4 @@ public interface UserMapper {
     UserDto mapEntityToDto(UserEntity entity, @Context CycleAvoidingContext context);
 
     UserEntity mapDtoToEntity(UserDto dto, @Context CycleAvoidingContext context);
-
-    UserImageDto mapImageEntityToImageDto(UserImageEntity imageEntity,  @Context CycleAvoidingContext context);
-    UserImageEntity mapImageDtoToImageEntity(UserImageDto imageDto,  @Context CycleAvoidingContext context);
 }
