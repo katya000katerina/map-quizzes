3661cd01a83dcf327273aae7e398e086be145b66
Katerina <krasnvasilk@gmail.com>
2024-06-20 22:40:25 +0300

    Change QuizService to QuizQuestionService

--- src/main/java/com/mapquizzes/controllers/QuizQuestionsRestController.java
+++ src/main/java/com/mapquizzes/controllers/QuizQuestionsRestController.java
@@ -3,7 +3,7 @@ package com.mapquizzes.controllers;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.mapquizzes.models.dto.QuizDto;
 import com.mapquizzes.models.dto.views.QuizViews;
-import com.mapquizzes.services.interfaces.QuizService;
+import com.mapquizzes.services.interfaces.QuizQuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class QuizQuestionsRestController {
 
-    private final QuizService service;
+    private final QuizQuestionService service;
 
     @JsonView(QuizViews.WithQuestions.class)
     @GetMapping("/{quizId}")
     public ResponseEntity<QuizDto> getQuizWithQuestions(@PathVariable Integer quizId) {
-        return ResponseEntity.ok(service.getById(quizId));
+        return ResponseEntity.ok(service.getQuizByIdWithQuestions(quizId));
     }
 }
