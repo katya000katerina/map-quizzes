b36d91142946b9b5d74b6f965f60b09f7bdeb0cc
Katerina <krasnvasilk@gmail.com>
2024-07-23 19:02:50 +0300

    Make a record out of PrincipalRankingDto class

--- src/main/java/com/mapquizzes/models/dto/PrincipalRankingDto.java
+++ src/main/java/com/mapquizzes/models/dto/PrincipalRankingDto.java
@@ -1,13 +1,4 @@
 package com.mapquizzes.models.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PrincipalRankingDto {
-    private String quizName;
-    private int rank;
+public record PrincipalRankingDto(String quizName, int rank) {
 }
