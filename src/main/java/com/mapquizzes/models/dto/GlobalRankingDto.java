ca73651a53602334fe4226eb3d0427eeaf8c4c70
Katerina <krasnvasilk@gmail.com>
2024-07-23 19:05:47 +0300

    Make a record out of GlobalRankingDto class

--- src/main/java/com/mapquizzes/models/dto/GlobalRankingDto.java
+++ src/main/java/com/mapquizzes/models/dto/GlobalRankingDto.java
@@ -1,13 +1,4 @@
 package com.mapquizzes.models.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class GlobalRankingDto {
-    private String username;
-    private Integer timeInMillis;
+public record GlobalRankingDto(String username, Integer timeInMillis) {
 }
