27f595c33fce7720dd3bdad0e60cb69e59bdbf69
Katerina <krasnvasilk@gmail.com>
2024-08-01 11:50:53 +0300

    Add AllArgsConstructor annotation

--- src/main/java/com/mapquizzes/models/dto/AuthenticationDto.java
+++ src/main/java/com/mapquizzes/models/dto/AuthenticationDto.java
@@ -1,11 +1,13 @@
 package com.mapquizzes.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpHeaders;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class AuthenticationDto {
     private UserDto userDto;
     private HttpHeaders tokenCookiesHeaders;
